e61b3badea316f7cb64cc639ffb807087d81c437
oywz <oywzzc@126.com>
2024-07-25 11:29:26 +0800

    add post route for create user

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use axum::{
-    routing::get,
-    Router,
+    http::StatusCode, 
+    routing::{get, post}, 
+    Router, 
+    Json,
 };
 
 use tracing::info;
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
@@ -15,11 +18,12 @@ async fn main() {
     // );
 
     let app = Router::new()
-        .route("/hello", get(hello));
+        .route("/hello", get(hello))
+        .route("/user", post(create_user));
 
     let addr = tokio::net::TcpListener::bind("0.0.0.0:8888").await.unwrap();
     info!("=>> Listening on {:?}", addr);
-    
+
     axum::serve(addr, app)
         .await
         .unwrap();
@@ -28,4 +32,27 @@ async fn main() {
 async fn hello() -> &'static str {
     "Hello world!"
 }
+
+async fn create_user(
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    let user = User {
+        id: 1337,
+        name: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
 
