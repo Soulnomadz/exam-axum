0d9789ade41c8e4949ca86bf577fa12b3f4c053c
oywz <oywzzc@126.com>
2024-07-26 09:27:10 +0800

    add route params

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use axum::{
-    http::StatusCode, 
-    routing::{get, post}, 
-    Router, 
-    Json,
+    extract::{Path, Query}, http::StatusCode, response::{Html, IntoResponse}, routing::{get, get_service, post}, Json, Router
 };
 
 use tracing::info;
 use serde::{Deserialize, Serialize};
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
@@ -18,20 +16,47 @@ async fn main() {
     // );
 
     let app = Router::new()
-        .route("/hello", get(hello))
-        .route("/user", post(create_user));
+        .merge(routes_hello())
+        .route("/user", post(create_user))
+        .fallback_service(routes_static());
 
     let addr = tokio::net::TcpListener::bind("0.0.0.0:8888").await.unwrap();
-    info!("=>> Listening on {:?}", addr);
+    info!("==>> Listening on {:?}", addr);
 
     axum::serve(addr, app)
         .await
         .unwrap();
 }
 
-async fn hello() -> &'static str {
-    "Hello world!"
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
 }
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
+
+fn routes_static() -> Router {
+    Router::new()
+        .nest_service("/", get_service(ServeDir::new("./")))
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    info!("==>> {:<12} - handler_hello - {params:?}", "HANDLER");
+    
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    info!("==>> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
 
 async fn create_user(
     Json(payload): Json<CreateUser>,
